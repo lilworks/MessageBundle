10247209ab4541405c9bf0c29ff9ae9cff25bfa8
lil-works <lilian@lil-works.com>
2017-10-13 07:21:58 +0200

    Add controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,12 +2,48 @@
 
 namespace Lilworks\MessageBundle\Controller;
 
+use Lilworks\MessageBundle\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+
         return $this->render('LilworksMessageBundle:Default:index.html.twig');
+    }
+    public function formAction(Request $request)
+    {
+        $message = new Message();
+        $form = $this->createForm('Lilworks\MessageBundle\Form\MessageType', $message,array('user'=>$this->getUser()));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            if($this->getUser()){
+                $message->setEmail($this->getUser()->getEmail());
+            }
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute('lil_works_message_sent', array(
+                'id' => $message->getId()
+            ));
+        }
+
+        return $this->render('LilworksMessageBundle:Default:form.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+
+    }
+
+    public function sentAction(Message $message)
+    {
+
+        return $this->render('LilworksMessageBundle:Default:sent.html.twig',array(
+            'message'=>$message
+        ));
     }
 }
---
+++ Form/MessageType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Lilworks\MessageBundle\Form;
+
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $user = $options["user"];
+
+        $builder
+            ->add('messageSubject')
+            ->add('message')
+            ->add('getCopy')
+        ;
+        if(!$user){
+            $builder->add('email',EmailType::class,array(
+                'required'=>true
+            ));
+        }
+        $builder->add('send', SubmitType::class, array(
+            'attr' => array('class' => 'send'),
+        ));
+
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Lilworks\MessageBundle\Entity\Message',
+            'user'=>null,
+            'translation_domain' => 'lilworks_message'
+        ));
+    }
+}
